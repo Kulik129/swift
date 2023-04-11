e77512e9d41b46336cf816e9524f704ccba5f148
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-04-11 21:49:01 +0300

    ДЗ первого семинара

---
+++ lesson1.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+ 
+let constanta: String = "- Это константа"
+let a: Int = 5
+let b: String = "a"
+let c: Double = 5.5
+let d: Bool = true
+let e: Character = "w"
+
+var peremennaya: String = "var - Это переменная"
+var f: Int = 4
+var g: String = "d"
+var h: Double = 4.4
+var i: Bool = false
+var j: Character = "e"
+
+//
+//let aa = 10
+//var bb = 0
+//if aa >= 7{
+//    bb+=7
+//    print(bb)
+//} else{
+//    bb-=7
+//    print(bb)
+//}
+//
+//if aa + bb > 15 && bb>=7 {
+//    bb *= 2
+//    print(bb)
+//}else{
+//    bb+=3
+//    print(bb)
+//}
+//
+//switch aa{
+//case 0...7: print(aa+bb)
+//case 8...20: print(aa-bb)
+//case 21...59: print(aa*bb)
+//default: print("noooo")
+//}
+
+for i in 0...101 where i % 2 != 0{
+    print(i)
+}
+
+
